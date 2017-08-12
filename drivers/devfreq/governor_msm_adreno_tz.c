eab20c699fef170c8c255887a05a696f5efa943c
flar2 <asegaert@gmail.com>
2017-08-12 16:50:39 +0530

    msm_adreno_tz: add adrenoboost parameter

--- drivers/devfreq/governor_msm_adreno_tz.c
+++ drivers/devfreq/governor_msm_adreno_tz.c
@@ -392,6 +392,15 @@ static int tz_get_target_freq(struct devfreq *devfreq, unsigned long *freq,
 		priv->bin.busy_time += stats.busy_time;
 	}	
 
+	priv->bin.total_time += stats.total_time;
+
+	// scale busy time up based on adrenoboost parameter, only if MIN_BUSY exceeded...
+	if ((unsigned int)(priv->bin.busy_time + stats.busy_time) >= MIN_BUSY) {
+		priv->bin.busy_time += stats.busy_time * (1 + (adrenoboost*3)/2);
+	} else {
+		priv->bin.busy_time += stats.busy_time;
+	}	
+
 	priv->bin.busy_time += stats.busy_time;
 
 	if (stats.private_data)
